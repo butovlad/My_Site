679610b46f969e34859dc1a65610e7df1981ae5f
butovlad <butov18@mail.ru>
2021-03-29 19:10:52 +0300

    4 filed edded

---
+++ script.js
@@ -0,0 +1,8 @@
+let message = document.querySelector('.subscription-message');
+let button = document.querySelector('.button-click');
+let name = document.querySelector('.subscription-name');
+let age = document.querySelector('.subscription-age');
+button.onclick = function() {
+  message.textContent = 'Здравствуйте, ' + name.value + '! ' +
+  'Ваш вораст: ' + age.value;
+};
